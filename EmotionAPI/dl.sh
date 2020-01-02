83cf614fff1f5719185f8951a6ab8d256fa8d9d1
Keshav Joshi <kjoshfree@gmail.com>
2020-01-02 15:00:55 +0530

    include file links

--- EmotionAPI/dl.sh
+++ EmotionAPI/dl.sh
@@ -1,5 +1,5 @@
 # can't copy .env files to heroku
 # . .env
 
-wget Tfidf_preprocessor
-wget best_model_mlp
+wget --no-check-certificate "https://onedrive.live.com/download?cid=C9414C674166322A&resid=C9414C674166322A%213453&authkey=AC_LMUh47nvkWyw"
+wget --no-check-certificate "https://onedrive.live.com/download?cid=C9414C674166322A&resid=C9414C674166322A%213454&authkey=AJB04pAaqxQ0-wc"
